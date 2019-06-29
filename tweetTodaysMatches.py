73392bff66e93666d7642049e2efc3643835c76f
Futa Nagashima <aruimimiura0@gmail.com>
2019-06-29 19:00:00 +0900

    lambda deploy

--- tweetTodaysMatches.py
+++ tweetTodaysMatches.py
@@ -6,14 +6,14 @@ from requests_oauthlib import OAuth1Session
 import matchEntity
 from getTodaysMatches import getTodaysMatches
 
-CK      = config.CONSUMER_KEY
-CS      = config.CONSUMER_SECRET
-AT      = config.ACCESS_TOKEN
-ATS     = config.ACCESS_TOKEN_SECRET
-# CK      = os.environ['CONSUMER_KEY']
-# CS      = os.environ['CONSUMER_SECRET']
-# AT      = os.environ['ACCESS_TOKEN']
-# ATS     = os.environ['ACCESS_TOKEN_SECRET']
+# CK      = config.CONSUMER_KEY
+# CS      = config.CONSUMER_SECRET
+# AT      = config.ACCESS_TOKEN
+# ATS     = config.ACCESS_TOKEN_SECRET
+CK      = os.environ['CONSUMER_KEY']
+CS      = os.environ['CONSUMER_SECRET']
+AT      = os.environ['ACCESS_TOKEN']
+ATS     = os.environ['ACCESS_TOKEN_SECRET']
 twitter = OAuth1Session(CK, CS, AT, ATS)
 
 URL = 'https://api.twitter.com/1.1/statuses/update.json'
@@ -31,7 +31,7 @@ def getTweet():
         text = '\n'.join(texts)
     return text
     
-# def lambda_handler(event, context):
-session = OAuth1Session(CK, CS, AT, ATS)
-params = {"status": getTweet() }
-req = session.post(URL, params = params)
+def lambda_handler(event, context):
+    session = OAuth1Session(CK, CS, AT, ATS)
+    params = {"status": getTweet() }
+    req = session.post(URL, params = params)
