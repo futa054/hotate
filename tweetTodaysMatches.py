c6f32ed65680e56b86e94f72366e795a442f09b3
Futa Nagashima <aruimimiura0@gmail.com>
2019-06-29 18:11:09 +0900

    add library

--- tweetTodaysMatches.py
+++ tweetTodaysMatches.py
@@ -6,10 +6,14 @@ from requests_oauthlib import OAuth1Session
 import matchEntity
 from getTodaysMatches import getTodaysMatches
 
-CK      = config.CONSUMER_KEY
-CS      = config.CONSUMER_SECRET
-AT      = config.ACCESS_TOKEN
-ATS     = config.ACCESS_TOKEN_SECRET
+# CK      = config.CONSUMER_KEY
+# CS      = config.CONSUMER_SECRET
+# AT      = config.ACCESS_TOKEN
+# ATS     = config.ACCESS_TOKEN_SECRET
+CK      = os.environ['CONSUMER_KEY']
+CS      = os.environ['CONSUMER_SECRET']
+AT      = os.environ['ACCESS_TOKEN']
+ATS     = os.environ['ACCESS_TOKEN_SECRET']
 twitter = OAuth1Session(CK, CS, AT, ATS)
 
 URL = 'https://api.twitter.com/1.1/statuses/update.json'
@@ -28,10 +32,6 @@ def getTweet():
     return text
     
 def lambda_handler(event, context):
-    r = requests.get(os.environ['API_ENDPOINT']+"?rand")
-    data = r.json()
     session = OAuth1Session(CK, CS, AT, ATS)
-    
     params = {"status": getTweet() }
-    session.post(URL, params = params)
-
+    req = session.post(URL, params = params)
